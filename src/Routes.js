8391c39171a7ba8599b06aebc9814dc01c1dd846
thegreatsantini <lss13@my.fsu.edu>
2018-11-19 20:34:27 -0800

    users can create a new note

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,18 @@
 import React from "react";
 import {Route, Switch } from "react-router-dom";
-import AppliedRoute from './components/AppliedRoute'
+import AppliedRoute from './components/AppliedRoute';
 import Home from "./containers/Home";
 import Login from "./containers/Login";
-import NotFound from './containers/NotFound'
+import Signup from './containers/Signup';
+import NewNote from "./containers/NewNote";
+import NotFound from './containers/NotFound';
 
 export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
+        <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+        <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
         { /* Finally, catch all unmatched routes */}
         <Route component={NotFound} />
     </Switch>;
--- src/config.js
+++ src/config.js
@@ -12,5 +12,6 @@ export default {
         USER_POOL_ID: process.env.REACT_APP_COGNITO_USER_POOL_ID,
         APP_CLIENT_ID: process.env.REACT_APP_COGNITO_APP_CLIENT_ID,
         IDENTITY_POOL_ID: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID
-    }
+    },
+    MAX_ATTACHMENT_SIZE: 5000000
 };
